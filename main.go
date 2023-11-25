0f0183236a25e6b06883b3ed381643b8eca0ec09
BlueBox <pongsapukanuvech@gmail.com>
2023-11-25 23:37:57 +0700

    [FIX] บาป

--- main.go
+++ main.go
@@ -12,14 +12,14 @@ func main() {
 	router := gin.New()
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK,
-			gin.H{"output": output},
+			output,
 		)
 	})
 	router.PATCH("/change/:name", func(c *gin.Context) {
 		name := c.Param("name")
 		output = name
 		c.JSON(http.StatusOK,
-			gin.H{"output": output},
+			output,
 		)
 	})
 
